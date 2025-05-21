b73d64a269b4116cffbc3e039bc300280eda36ef
Denys Smelnytskyi <deniska1406sme@gmail.com>
2025-05-21 21:44:20 +0300

    fixed name

--- src/main/java/com/gmail/deniska1406sme/sceduleservice/ScheduleServiceApplication.java
+++ src/main/java/com/gmail/deniska1406sme/sceduleservice/ScheduleServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SceduleServiceApplication {
+public class ScheduleServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SceduleServiceApplication.class, args);
+        SpringApplication.run(ScheduleServiceApplication.class, args);
     }
 
 }
